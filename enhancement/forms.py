be0bfb754d6b76f35176f678744d112e94e3a566
tantowijh <tjmailone@gmail.com>
2024-11-21 15:30:30 +0700

    Limit enhancement size

--- enhancement/forms.py
+++ enhancement/forms.py
@@ -1,5 +1,15 @@
 from django import forms
 from django.conf import settings
+from django.core.exceptions import ValidationError
 
 class ImageUploadForm(forms.Form):
     image = forms.ImageField()
+
+    def clean_image(self):
+        image = self.cleaned_data.get('image')
+        max_size = 5 * 1024 * 1024  # 5 MB limit
+
+        if image.size > max_size:
+            raise ValidationError(f"Image file too large ( > {max_size / (1024 * 1024)} MB )")
+        
+        return image
